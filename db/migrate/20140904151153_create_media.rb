7b266804b30e6cb9b74cc5ded7fef4ba10dacd6e
Grace Yim <graceekim@gmail.com>
2014-09-04 10:13:18 -0500

    created media model and migration

---
+++ db/migrate/20140904151153_create_media.rb
@@ -0,0 +1,15 @@
+class CreateMedia < ActiveRecord::Migration
+  def change
+    create_table :media do |t|
+      t.string :rottentomatoes_id
+      t.string :imdb_id
+      t.string :wikipedia_id
+      t.integer :run_time
+      t.integer :rating
+      t.text :synopsis
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
