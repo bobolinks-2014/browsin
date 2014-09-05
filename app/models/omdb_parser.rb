497392e731d0acdef3e1d9397e9a5f0bb20263ae
Joey Chamberlin <joey.chamberlin@gmail.com>
2014-09-05 11:50:09 -0500

    Parser now preps return for RTParser

--- app/models/omdb_parser.rb
+++ app/models/omdb_parser.rb
@@ -2,23 +2,35 @@ require 'net/http'
 require 'json'
 
 class OMDBParser
-  def self.get_uri
-    "http://www.omdbapi.com/?i=#{@imdb_id}"
+  def self.make_omdb_calls(file)
+    file.each do |id| #1500 times
+      response = Net::HTTP.get("http://www.omdbapi.com/?i=#{id.imdb_id}")
+      hashed = JSON.parse(response)
+      omdb_collection = []
+      omdb_data = {
+        imdb_id: imdb_id,
+        rt_id: rt_id
+        title: hashed["Title"],
+        runtime: hashed["Runtime"],
+        genre_list: hashed["Genre"],
+        actor_list: hashed["Actors"],
+        synopsis: hashed["Plot"],
+        type: hashed["Type"],
+        if type != ["Movie"]
+          imdb_rating: hashed["imdbRating"]
+        end
+      omdb_collection << omdb_data
+    end
+    }
   end
 
-  def self.get_omdb_json
-    response = Net::HTTP.get(get_uri)
-    hashed = JSON.parse(response)
-    omdb_data = {
-      title: hashed["Title"],
-      runtime: hashed["Runtime"],
-      genre_list: hashed["Genre"],
-      actor_list: hashed["Actors"],
-      synopsis: hashed["Plot"],
-      # type: hashed["Type"],  **Benchmark
-      # if type != ["Movie"]
-      imdb_rating: hashed["imdbRating"]
-      # end
-    }
+  def self.update_media
+    Media.find_by(imdb_id).update(omdb_data)
+    pass_to_rt = []
+    omdb_collection.map do |omdb_data|
+    if omdb_data.key(nil)
+      2.times {omdb_data.shift}
+      pass_to_rt << omdb_data
+    end
   end
 end
