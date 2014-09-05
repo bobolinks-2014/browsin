3b1a3445326f33d4c79c61fa91b9f1815dae2701
Joey Chamberlin & Rob Schwartz <pair+jochambo.robschwartz@devbootcamp.com>
2014-09-05 14:03:07 -0500

    Added regex and search examples

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,20 @@
 class SearchController < ApplicationController
 
   def search
-    movies = Media.tagged_with(tag_key, on: search_value.to_sym).tagged_with("show", on: :status, owned_by: current_user) 
+    # {query=value}
+    # {"genres=comedy"}
+    # {"i have 30 minutes for comedy"}
+    # {"i have 60 minutes for Tom Cruise"}
+    # {"Tom Cruise, comedy"}
+    # {"20 min, comedy"}
+
+    # /(\d+|\bcomedy|\bTom Cruise)/i
+
+    # tag_key = "search value, like comedy"
+    # search_value = "taggable, like genres"
+    # Media.tagged_with(["comedy", "30 mins"]).tagged_with()
+
+    movies = Media.tagged_with(tags).tagged_with("show", on: :status, owned_by: current_user)
     if movies == []
       respond_with movies, status: 404
     else
@@ -10,11 +23,11 @@ class SearchController < ApplicationController
   end
 
   private
-  
+
   def search_value
     params.keys[0]
   end
-  
+
   def tag_key
     params.values[0]
   end
