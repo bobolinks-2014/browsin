aa42adacea4c5d1d099244a59e1c85041bb1f6e2
robschwartz <robertschwar@gmail.com>
2014-09-05 17:42:52 -0500

    added search controller features and parsing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,7 @@
 class SearchController < ApplicationController
 
   def search
-    # {query=value}
-    # {"genres=comedy"}
-    # {"i have 30 minutes for comedy"}
-    # {"i have 60 minutes for Tom Cruise"}
-    # {"Tom Cruise, comedy"}
-    # {"20 min, comedy"}
-
-    # /(\d+|\bcomedy|\bTom Cruise)/i
-
-    # tag_key = "search value, like comedy"
-    # search_value = "taggable, like genres"
-    # Media.tagged_with(["comedy", "30 mins"]).tagged_with()
-
-<<<<<<< HEAD
     movies = Media.tagged_with(tags).tagged_with("show", on: :status, owned_by:current_user)
-=======
-    movies = Media.tagged_with(tags).tagged_with("show", on: :status, owned_by: current_user)
->>>>>>> 3b1a3445326f33d4c79c61fa91b9f1815dae2701
     if movies == []
       respond_with movies, status: 404
     else
@@ -72,6 +55,6 @@ class SearchController < ApplicationController
   end
 
   def params_query
-    [:query]
+    params[:query]
   end
 end
