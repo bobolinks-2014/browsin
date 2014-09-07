6449b1b61ccfad47be76694c62cbc3ce2cdaabbb
Grace Yim <graceekim@gmail.com>
2014-09-07 00:06:55 -0500

    updated omdb parser to handle netflix data

--- app/models/omdb_parser.rb
+++ app/models/omdb_parser.rb
@@ -2,28 +2,47 @@ require 'net/http'
 require 'json'
 
 class OMDBParser
-  def self.get_api_data(imdb_ids)
+  def self.get_api_data(media_data, netflix)
     omdb_collection = []
 
-    imdb_ids.each do |id| #1500 times
-      uri = URI("http://www.omdbapi.com/?i=#{id}&tomatoes=true")
+    media_data.each do |id| #1500 times
+      if netflix == true
+        url = URI.encode("http://www.omdbapi.com/?t=#{id}&tomatoes=true")
+        uri = URI.parse(url)
+      else
+        uri = URI("http://www.omdbapi.com/?i=#{id}&tomatoes=true")
+      end
+      
       response = Net::HTTP.get_response(uri)
+      api_data = response.body
       hashed = JSON.parse(response.body)
 
-      omdb_data = { imdb_id: id,
-                    title: hashed["Title"],
-                    run_time: hashed["Runtime"],
-                    genre_list: hashed["Genre"],
-                    actor_list: hashed["Actors"],
-                    synopsis: hashed["Plot"],
-                    type: hashed["Type"]
-                  }
+      if hashed["Response"] == false
+        break
+      else 
+        omdb_data = { imdb_id: id,
+                      title: hashed["Title"],
+                      run_time: hashed["Runtime"],
+                      genre_list: hashed["Genre"],
+                      actor_list: hashed["Actors"],
+                      synopsis: hashed["Plot"],
+                      type: hashed["Type"]
+                    }
+        if netflix == true 
+          omdb_data.merge!(:imdb_id => hashed["imdbID"], :service_list => "netflix")
+          if hashed["Type"] == "movie"
+            omdb_data.merge!(:platform_list => "movie")
+          else
+            omdb_data.merge!(:platform_list => "shows")
+          end
+        end
 
-      if omdb_data[:type] == "movie"
-        omdb_data.merge!(:rating => hashed["tomatoUserMeter"])
-      else
-        imdb_rating = hashed["imdbRating"].to_f * 10
-        omdb_data.merge!(:rating => imdb_rating)
+        if omdb_data[:type] == "movie"
+          omdb_data.merge!(:rating => hashed["tomatoUserMeter"])
+        else
+          imdb_rating = hashed["imdbRating"].to_f * 10
+          omdb_data.merge!(:rating => imdb_rating)
+        end
       end
 
       omdb_data.delete(:type)
@@ -32,11 +51,22 @@ class OMDBParser
     return omdb_collection
   end
 
-  def self.update_media(imdb_data)
+  def self.create_update_media(imdb_data)
     imdb_data.each do |media|
       current_media = Media.find_by_imdb_id(media[:imdb_id])
-      media.delete(:imdb_id)
-      current_media.update(media)
+      if current_media == nil
+        new_media = Media.new(media)
+        if new_media.save
+          break
+        else
+          old_media = Media.find_by_imdb_id(media[:title])
+          old_media.service_list.add("netflix")
+          old_media.save
+        end
+      else
+        media.delete(:imdb_id)
+        current_media.update(media)
+      end
     end
   end
 
