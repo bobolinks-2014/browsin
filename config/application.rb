e22eef0c588d529462b1f8e1077ad3f56f6b6670
Joey Sabani <joey.sabani@gmail.com>
2014-09-07 15:53:53 -0500

    font issue and asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module BrowsinRails
     config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.serve_static_assets = true
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
