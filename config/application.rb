b15cc2cf797f9d57e59205417836b73dfe0203c1
Joey Sabani <joey.sabani@gmail.com>
2014-09-07 18:36:15 -0500

    font help

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ Bundler.require(*Rails.groups)
 module BrowsinRails
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "images")
     config.serve_static_assets = true
     config.assets.precompile << (/\.(?:svg|eot|woff|ttf)$/)
     # Settings in config/environments/* take precedence over those specified here.
