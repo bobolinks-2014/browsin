7ca50dd1a91231a246a19a969f9bff12fc6bbc64
Grace Yim <graceekim@gmail.com>
2014-09-08 12:10:27 -0500

    refactored search and user controllers for speed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,63 @@ class SearchController < ApplicationController
   attr_accessor :matches
 
   def search
-    movies = Media.where("run_time <= #{runtime_search}").tagged_with(@matches).tagged_with("show", on: :status, owned_by:current_user).order('rating DESC').limit(25)
-    if movies == []
+    find_results
+    if @movies == []
       render json: {success: false, error: params_query}
     else
-      render json: movies, include: [:genres, :services, :actors]
+      render json: @movies, include: [:genres, :services, :actors]
     end
+  end
+  
+  def remove 
+    UserPreference.create(user_id: current_user.id, media_id: params[:id], view_status: "hidden")
+    render json: {success: true}
+  end
+  
+  def top
+    if user_signed_in?
+      render json: top_list, include: [:genres, :services, :actors]
+    else
+      render json: {success: false}
+    end
+  end
+
+  def find
+    render json: find_media(params[:lookup]), include: [:genres, :services, :actors]
+  end
+
+  private
+
+  def find_media(matcher)
+      current_user_media.tagged_with(matcher).order('rating DESC, title ASC').limit(25)
+  end
+
+  def params_query
+    params[:query].downcase
+  end
+
+  def top_list
+    current_user_media.order('rating DESC, title ASC').limit(25)
+  end
+
+  def find_results
+    get_matches
+    if is_number? && is_only_number?  
+      movies = current_user_media.where("run_time <= #{runtime_search}")
+    elsif is_number?
+      movies = current_user_media.tagged_with(@matches).where("run_time <= #{runtime_search}")
+    else
+      movies = current_user_media.tagged_with(@matches)
+    end
+    @movies = movies.order('rating DESC, title ASC').limit(25)
+  end
+
+  def current_user_media
+    Media.tagged_with(current_user.service_list, :any => true).where.not(title: hidden_media)
+  end
+
+  def hidden_media
+    current_user.hidden_media.pluck(:title)
   end
 
   def re_actors
@@ -20,23 +71,20 @@ class SearchController < ApplicationController
     Regexp.union(@genre_list)
   end
 
-  # single regex to produce search groups
   def get_matches
-    query = params_query.split.each {|x| x.downcase!}.join(" ")
     m = params_query.scan(/(\d+)|(#{re_actors})|(#{re_genres})/i)
-    m.flatten!.compact!.sort!
+    @matches = m.flatten.compact.sort
   end
 
   def runtime_search
-    @matches = get_matches
     @matches.shift.to_i
   end
 
-  # Need to compare actor against acts_as_taggable_on actor_list
-
-  private
-
-  def params_query
-    params[:query]
+  def is_number?
+    @matches[0].to_i > 0
   end
+  
+  def is_only_number?
+    @matches.length == 1
+  end 
 end
