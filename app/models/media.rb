24eb37e681e78a5a09b6d6b323a6e325c7b320c8
Grace Yim <graceekim@gmail.com>
2014-09-08 12:11:04 -0500

    refactored user and media models for speed

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,6 +1,18 @@
 class Media < ActiveRecord::Base
+  has_many :user_preferences
+  # has_many :hidden_users, -> {where view_status: "hidden"}, through: :user_preferences, source: :users
+  has_many :hidden_users, through: :user_preferences, source: :users
+
 	acts_as_taggable_on :services, :genres, :platforms, :actors, :statuses
 
 	# validates presence of tags services and platform (cant be empty)
 	validates_presence_of :imdb_id, :service_list, :platform_list
+	validates_uniqueness_of :imdb_id
+
+	def self.clear_incomplete_records
+		self.where(rating: 0).destroy_all
+		self.where(run_time: 0).destroy_all		
+		self.where(rating: nil).destroy_all
+		self.where(run_time: 0).destroy_all
+	end
 end
