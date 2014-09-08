af23c6909cb265396fbad55aec123e631c785316
Grace Yim <graceekim@gmail.com>
2014-09-08 00:44:02 -0500

    added more search controller tests

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 describe SearchController do 
-	let(:movie) {Media.create!(imdb_id: "testing", service_list: "hbo", platform_list: "shows", run_time: 50, actor_list: "Cersei, Arya, Brienne", rating: 100)}
-	let(:movie_all) {[movie].to_json(:include => [:genres, :services, :actors])}
+	let(:got) {Media.create!(imdb_id: "testing123", service_list: "hbo", platform_list: "shows", run_time: 50, actor_list: "Cersei, Arya, Brienne", rating: 100, genre_list: "Drama")}
+	let(:got_json) {[got].to_json(:include => [:genres, :services, :actors])}
+	let(:hoc) {Media.create!(imdb_id: "testing321", service_list: "netflix", platform_list: "shows", run_time: 50, actor_list: "Frank, Claire", rating: 90, genre_list: "Comedy")}
+	let(:hoc_json) {[hoc].to_json(:include => [:genres, :services, :actors])}
+	let(:all_movies) {[got, hoc].to_json(:include => [:genres, :services, :actors])}
 	let(:user) {User.create!(email: "testing@testing.com", password: "testing", password_confirmation: "testing")}
 
 	describe 'search route' do 
 		before(:each) do
-			user.tag(movie, with: :"show", on: :status)
-			allow(movie).to receive(:runtime_search){30} 
+			user.tag(got, with: :"show", on: :status)
+			user.tag(hoc, with: :"show", on: :status)
+			allow(got).to receive(:runtime_search){30} 
+			allow(hoc).to receive(:runtime_search){30} 
 			allow(SearchController).to receive(:current_user){ user }
 		end
 
@@ -19,11 +24,11 @@ describe SearchController do
 			expect(response).to be_success
 		end
 
-		it "search for '60 minutes' returns a single movie" do
+		it "search for '60 minutes' returns two movies" do
 			get(:search,
 				:query => "60 minutes",
 				:format => 'json')
-			expect(response.body).to eq(movie_all)
+			expect(response.body).to eq(all_movies)
 		end
 
 		it "search for '20 minutes' returns no movies" do
@@ -33,6 +38,19 @@ describe SearchController do
 			expect(response.body).to eq({success: false, error: '20 minutes'}.to_json)
 		end
 
+		it "search for 'drama' returns one movie" do
+			get(:search,
+				:query => "drama",
+				:format => 'json')
+			expect(response.body).to eq(got_json)
+		end
+
+		it "search for '60 minutes and comedy' returns one movie" do
+			get(:search,
+				:query => "60 minutes and comedy",
+				:format => 'json')
+			expect(response.body).to eq(hoc_json)
+		end
 
 	end
 end
