726202cff9416d67974fc7bfe4b39b57066666d6
Grace Yim <graceekim@gmail.com>
2014-09-08 20:33:43 -0500

    updated specs

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,16 +6,17 @@ describe SearchController do
 	let(:hoc) {Media.create!(imdb_id: "testing321", service_list: "netflix", platform_list: "shows", run_time: 50, actor_list: "Frank, Claire", rating: 90, genre_list: "Comedy")}
 	let(:hoc_json) {[hoc].to_json(:include => [:genres, :services, :actors])}
 	let(:all_movies) {[got, hoc].to_json(:include => [:genres, :services, :actors])}
-	let(:user) {User.create!(email: "testing@testing.com", password: "testing", password_confirmation: "testing")}
+	let(:user) {User.create!(email: "testing@testing.com", password: "testing", password_confirmation: "testing", service_list: "hbo")}
 	let(:error) {{success: false, error: '20 minutes'}.to_json}
+	let(:preference){UserPreference.create!(user_id: user.id, media_id: got.id, view_status: "show")}
 
 	describe 'search route' do 
 		before(:each) do
-			user.tag(got, with: :"show", on: :status)
-			user.tag(hoc, with: :"show", on: :status)
 			allow(got).to receive(:runtime_search){50} 
 			allow(hoc).to receive(:runtime_search){50} 
 			allow(SearchController).to receive(:current_user){user}
+			(SearchController).stub_chain('current_user.service_list').and_return(user.service_list)
+
 		end
 
 		it "search for '60 minutes' gets a successful response" do
@@ -52,5 +53,77 @@ describe SearchController do
 				:format => 'json')
 			expect(response.body).to eq(hoc_json)
 		end
+
+		it "search for Frank returns one movie" do
+			get(:search,
+				:query => "60 minutes for Frank",
+				:format => 'json')
+			expect(response.body).to eq(hoc_json)
+		end
+
+		it "search for Jim returns zero results" do
+			get(:search,
+					:query => "30 minutes for Jim",
+					:format => 'json')
+			expect(response.body).should_not eq(all_movies)
+		end
+
+		it "a search for Frank or Arya returns got and hoc doesn't compute" do
+			get(:search,
+					:query => "60 minutes for Frank and Arya",
+					:format => 'json')
+			expect(response.body).should_not eq(all_movies)
+		end		
+	end
+
+	describe 'remove route' do
+		before(:each) do
+			user = User.create!(email: "testing@testing.com", password: "testing", password_confirmation: "testing", service_list: "hbo")
+			allow(got).to receive(:runtime_search){50} 
+			allow(hoc).to receive(:runtime_search){50} 
+			allow(SearchController).to receive(:current_user){user}
+			allow(UserPreference).to receive(:create){preference}
+
+			# allow(SearchController).to receive('UserPreference.create')
+		end
+# testing remove method
+		it "user is able to hide a show from their list" do
+			SearchController.stub_chain('UserPreference.create'){preference}
+			patch(:remove, 
+					:id => hoc.id,
+					:format => 'json')
+			media = Media.find(hoc.id)
+			expect(user.hidden_media.count).to eq(1)
+		end
+# testing top method --> tested in feature testing 
+
+		# it "clicking the play button give the top 25 rated media items" do
+		# 	get(:top,
+		# 			:query => ,
+		# 			:format => 'json')
+		# 	expect(response.body).to eq(all_movies)
+		# end
+
+		# it 'clicking play after removing a show does not show that item' do
+		# 	put(:remove, 
+		# 			#remove got
+		# 			:query => ,
+		# 			:format => 'json')
+
+		# 	get(:search,
+		# 			:query => ,
+		# 			:format => 'json')
+		# 	expect(response.body).to eq(hoc_json)
+
+		# end
+
+# testing find method
+		it 'finding comedy returns hoc' do
+			get(:find,
+					:lookup => "comedy",
+					:format => 'json')
+			expect(response.body).to eq(hoc_json)
+		end
+>>>>>>> Stashed changes
 	end
 end
