b0ae67a81e1ef8e1351f44fe676995749ba6bf95
Grace Yim <graceekim@gmail.com>
2014-09-11 09:07:06 -0500

    refactored search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,25 +3,22 @@ class SearchController < ApplicationController
 
   def search
     results = Search.find_results(current_user, params[:query])
-    matches = results[:matches]
-    movies = results[:movies]
-    if movies == []
+    if results[:movies] == []
       render json: {success: false, error: params[:query]}
     else
-      render json: {media: movies.to_json(include: :actors, methods: [:genre_icons, :service_icons]), matches: matches}
+      render json: {media: results[:movies].to_json(include: :actors, methods: [:genre_icons, :service_icons, :rating_source]), matches: results[:matches]}
     end
   end
 
   def top
     if user_signed_in?
-      render json: {media: Search.top_list(current_user).to_json(include: :actors, methods: [:genre_icons, :service_icons]), matches: ["top 25 movies based on your available services"]}
+      render json: {media: Search.top_list(current_user).to_json(include: :actors, methods: [:genre_icons, :service_icons, :rating_source]), matches: ["top 25 movies based on your available services"]}
     else
       render json: {success: false}
     end
   end
 
-  def find
-    render json: {media: Search.find_media(current_user, params[:lookup]).to_json(include: :actors, methods: [:genre_icons, :service_icons]), matches: [params[:lookup]]}
+  def filter
+    render json: {media: Search.find_media(current_user, params[:lookup]).to_json(include: :actors, methods: [:genre_icons, :service_icons, :rating_source]), matches: [params[:lookup]]}
   end
-
 end
