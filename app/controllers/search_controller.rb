aba77efe8364efb13cb3038b7dd01744afc337b3
Grace Yim <graceekim@gmail.com>
2014-09-11 12:54:24 -0500

    refactored with presenter pattern

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,19 +6,19 @@ class SearchController < ApplicationController
     if results[:movies] == []
       render json: {success: false, error: params[:query]}
     else
-      render json: {media: results[:movies].to_json(include: :actors, methods: [:genre_icons, :service_icons, :rating_source]), matches: results[:matches]}
+      render json: {media: results[:movies].collect {|media| SearchResultPresenter.new(media).to_hash}.to_json, matches: results[:matches]}
     end
   end
 
   def top
     if user_signed_in?
-      render json: {media: Search.top_list(current_user).to_json(include: :actors, methods: [:genre_icons, :service_icons, :rating_source]), matches: ["top 25 movies based on your available services"]}
+      render json: {media: Search.top_list(current_user).collect {|media| SearchResultPresenter.new(media).to_hash}.to_json, matches: ["top 25 movies based on your available services"]}
     else
       render json: {success: false}
     end
   end
 
   def filter
-    render json: {media: Search.find_media(current_user, params[:lookup]).to_json(include: :actors, methods: [:genre_icons, :service_icons, :rating_source]), matches: [params[:lookup]]}
+    render json: {media: Search.find_media(current_user, params[:lookup]).collect {|media| SearchResultPresenter.new(media).to_hash}.to_json, matches: [params[:lookup]]}
   end
 end
